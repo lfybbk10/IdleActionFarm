f1f85319a752ee0ee825c61fb2186cf252b8dfbe
lfybbk10 <63192458+lfybbk10@users.noreply.github.com>
2022-08-02 20:20:26 +0300

    create map
    Former-commit-id: a4fd01d889bb2a14fcc54f9f67bb0d2da2d505e6
    Former-commit-id: 859e3c2cc5088df5edffc75220a0863468300063

--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using DG.Tweening;
 
@@ -14,14 +15,21 @@ public class CharacterMoveController : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
+    private void Start()
+    {
+        float correctHeight = _characterController.center.y + _characterController.skinWidth;
+        _characterController.center = new Vector3(0, correctHeight, 0);
+    }
+
     private void Update()
     {
         if (_joystick.Direction != Vector2.zero)
         {
             _animator.SetBool("IsRunning",true);
             Vector3 joystickDir = new Vector3(_joystick.Direction.x,0,_joystick.Direction.y);
-            _characterController.Move(joystickDir * Time.deltaTime);
-            transform.DOLookAt(new Vector3(joystickDir.x + transform.position.x,0,joystickDir.z+transform.position.z), 0.1f);
+            _characterController.SimpleMove(joystickDir);
+            print(_characterController.isGrounded);
+            transform.DOLookAt(new Vector3(joystickDir.x + transform.position.x,0,joystickDir.z+transform.position.z), 0.1f,AxisConstraint.Y);
         }
         else
         {
